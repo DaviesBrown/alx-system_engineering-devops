353d09f45b8d3e50e6e0a21f9b170cb06defcc12
Davies Brown <nwosudavid77.com>
2023-06-24 02:17:43 +0100

    puppeteer

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 # excecute kill me now on loop bash script
 exec { 'killmenow':
-  command     => 'pkill killmenow',
-  path        => '/usr/bin:/usr/sbin:/bin',
-  refreshonly => true,
+  command => 'pkill killmenow',
+  path    => '/usr/bin',
+  onlyif  => 'pgrep killmenow',
 }
+
