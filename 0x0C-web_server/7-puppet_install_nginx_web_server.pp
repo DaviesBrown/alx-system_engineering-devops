05f2dbd111df805e3125a67c132790fb95fd5669
Davies Brown <nwosudavid77.com>
2023-07-03 02:52:49 +0100

    web-server-config

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,39 @@
+class nginx_config {
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  file { '/var/www/html/index.html':
+    ensure  => present,
+    content => 'Hello World!',
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure => present,
+    content => "
+      server {
+        listen 80;
+        root /var/www/html;
+        index index.html;
+        
+        location / {
+          try_files $uri $uri/ =404;
+        }
+        
+        location /redirect_me {
+          return 301 http://example.com/new_page;
+        }
+      }
+    ",
+    notify => Service['nginx'],
+  }
+
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+    hasrestart => true,
+  }
+}
+
+include nginx_config
