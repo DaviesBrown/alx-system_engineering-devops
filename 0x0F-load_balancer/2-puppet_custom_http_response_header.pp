3afd56cbce2d4ce172bbcbd0e54f9e12a9a8eb59
Davies Brown <nwosudavid77.com>
2023-07-04 03:21:56 +0100

    load-balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,16 @@
-# Add a custom HTTP header with Puppet
-exec { 'update':
+# Use Puppet to automate the task of creating a custom HTTP header response
+
+exec {'update':
   command => '/usr/bin/apt-get update',
-  path    => '/usr/bin',
-  before  => Package['nginx'],
 }
-
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['update'],
+-> package {'nginx':
+  ensure => 'present',
 }
-
-file_line { 'http_header':
-  path    => '/etc/nginx/nginx.conf',
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
   match => 'http {',
   line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
-  notify  => Exec['restart_nginx'],
-  require => Package['nginx'],
 }
-
-exec { 'restart_nginx':
-  command     => '/usr/sbin/service nginx restart',
-  path        => '/usr/bin',
-  refreshonly => true,
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
