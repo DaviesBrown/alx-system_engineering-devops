167724bd9a6da00dfdd419bb9260b4cbc1c2037f
Davies Brown <nwosudavid77.com>
2023-08-17 14:21:48 +0100

    open-file-limit-fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,7 +3,7 @@
 # Adjust the ULIMIT in the default configuration file
 exec { 'increase-nginx-ulimit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/bin/'
 }
 
 # Restart the Nginx service
